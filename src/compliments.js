5cb69849b028e39d32ae4c3212ac0c6e5c86cd91
Adam McKerlie <adammckerlie@gmail.com>
2016-02-29 12:05:19 -0500

    Added new compliments

--- src/compliments.js
+++ src/compliments.js
@@ -4,6 +4,16 @@ var compliments = [
 "People behind you at movies think you are the perfect height!",
 "Chuck Norris is intimidated by you!",
 "The only thing that shines brighter than you is the Sun, and only by a little bit!",
+"Loved ones say your smile is as joyful as the cool spot found on a pillow",
+"If friendships were donuts, you'd be the sprinkle kind, full of colour and wonder",
+"Your speech reaches the masses like wind chimes spreading nature's song",
+"Marine biologists imagine your heart to be as beautiful as the echo of a whale during mating season",
+"Doctors boast about the perfection that exudes from your gallbladder",
+"Daily commuters appreciate the way you signal in advance",
+"Truck drivers are grateful for your generosity in allowing them to merge onto the highway",
+"Photographers believe you could make a fortune in the modelling industry even without the need for filters and Photoshop",
+"Your stride is as graceful as a skateboard on smooth terrain",
+"Even the sound of church bells cannot beat the purity that exits your mouth during karaoke"
 ];
 
 module.exports = compliments;
